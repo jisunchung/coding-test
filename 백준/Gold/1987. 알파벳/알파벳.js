96b12fbf9e6ea2ac7deb1658edc9e0ba3d8a1257
jisunchung <65282581+jisunchung@users.noreply.github.com>
2024-07-12 16:08:14 +0900

    [Gold IV] Title: 알파벳, Time: 2304 ms, Memory: 11452 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.js
+++ 백준/Gold/1987. 알파벳/알파벳.js
@@ -1,14 +1,14 @@
 const [n, ...input] = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n")
 
 const [R, C] = n.split(" ").map(Number)
-const board = input.map(val => val.split(""))
+const board = input.map(val => val.split("").map(char => char.charCodeAt() - 'A'.charCodeAt()))
 const visited = new Array(26).fill(false)
 let max = 0
 const direction = [[-1, 0], [1, 0], [0, -1], [0, 1]]
 
 function possible(row, col) {
 		if (row < 0 || row >= R || col < 0 || col >= C) return false
-		if (visited[board[row][col].charCodeAt(0) - 'A'.charCodeAt(0)]) return false
+		if (visited[board[row][col]]) return false
 		return true
 }
 
@@ -21,13 +21,13 @@ function dfs(row, col, length) {
 				const nextRow = row + dir_r
 				const nextCol = col + dir_c
 				if (possible(nextRow, nextCol)) {
-						visited[board[nextRow][nextCol].charCodeAt(0) - 'A'.charCodeAt(0)] = true
+						visited[board[nextRow][nextCol]] = true
 						dfs(nextRow, nextCol, length + 1)
-						visited[board[nextRow][nextCol].charCodeAt(0) - 'A'.charCodeAt(0)] = false
+						visited[board[nextRow][nextCol]] = false
 				}
 		}
 }
 
-visited[board[0][0].charCodeAt(0) - 'A'.charCodeAt(0)] = true
+visited[board[0][0]] = true
 dfs(0, 0, 1)
 console.log(max)
