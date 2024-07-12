c4dc9b002117e5e8d73c31fa4de2aed60d49e4a4
jisunchung <65282581+jisunchung@users.noreply.github.com>
2024-07-12 23:44:18 +0900

    [Gold I] Title: 비숍, Time: 216 ms, Memory: 10000 KB -BaekjoonHub

--- 백준/Gold/1799. 비숍/비숍.js
+++ 백준/Gold/1799. 비숍/비숍.js
@@ -4,25 +4,30 @@ const N = Number(input.shift());
 const board = input.map(line => line.split(" ").map(Number));
 const diag1 = new Array(2 * N).fill(false); // \ 대각선 사용 여부
 const diag2 = new Array(2 * N).fill(false); // / 대각선 사용 여부
+let answer = [0, 0];
 
-function placeBishops(board, diag1, diag2, row, color) {
-	//채스판은 0~N-1까지의 index를 가짐. N이면 끝난것이기 때문에 추가된 비숍없음!
-	if (row === N) return 0
-
-	let result = 0;
-	for (let col = (row + color) % 2; col < N; col += 2) {
-			if (board[row][col] && !diag1[col - row + N] && !diag2[col + row]) {
-					diag1[col - row + N] = diag2[col + row] = true;
-				//현재 위치에 비숍을 배치하는 경우
-					result = Math.max(result, placeBishops(board, diag1, diag2, row, color, N) + 1);
-					diag1[col - row + N] = diag2[col + row] = false;
-			}
+function placeBishops(row, col, count, color) {
+	//열이 체스판을 넘어간 경우 다음행으로 넘어감
+	if (col >= N) {
+		row++;
+		//새로운 행의 시작 열 결정
+		col = (col % 2 === 0) ? 1 : 0;
 	}
-	//현재 위치에 비숍을 배치하지 않는경우
-	return Math.max(result, placeBishops(board, diag1, diag2, row + 1, color));
+	//행이 체스판 크기를 넘어간 경우 검사를 모두 했다는 것을 의미함
+	if (row >= N) {
+		answer[color] = Math.max(answer[color], count);
+		return;
+	}
+	if (board[row][col] && !diag1[col - row + N] && !diag2[col + row]) {
+			diag1[col - row + N] = diag2[col + row] = true;
+			//현재 행에 비숍을 배치하는 경우
+			placeBishops(row, col + 2, count + 1, color)
+			diag1[col - row + N] = diag2[col + row] = false;
+	}
+	//현재 행에 비숍을 배치하지 않고 다음행으로 이동
+	placeBishops(row, col + 2, count, color)
 }
 
-const blackResult = placeBishops(board, diag1, diag2, 0, 0); 
-const whiteResult = placeBishops(board, diag1, diag2, 0, 1); 
-console.log(blackResult + whiteResult);
-
+placeBishops(0,0,0,0); 
+placeBishops(0, 1 , 0, 1); 
+console.log(answer[0] + answer[1]);
