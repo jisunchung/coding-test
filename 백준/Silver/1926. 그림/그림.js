8f34393b5fcdd9ca8ed8940c896708353540880a
jisunchung <65282581+jisunchung@users.noreply.github.com>
2024-07-16 18:47:37 +0900

    [Silver I] Title: 그림, Time: 264 ms, Memory: 21168 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.js
+++ 백준/Silver/1926. 그림/그림.js
@@ -3,24 +3,21 @@ const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("
 const [n, m] = input.shift().split(" ").map(Number)
 const board = input.map(val => val.split(" ").map(Number))
 
-const visited = Array.from(Array(n), () => new Array(m).fill(false))
-const directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-
 function possible(row, col){
-	if(row < 0 || row >= n || col < 0 || col >= m) return false
-	return true
+	return row >= 0 && row < n && col >= 0 && col < m
 }
 
 function bfs(row, col){
 	let q = [[row,col]]
+	board[row][col] = 0
 	let size = 1
 	while(q.length !== 0){
-		let [curRow,curCol] = q.shift()
-		for(let [dirRow, dirCol] of directions){
+		let [curRow, curCol] = q.shift()
+		for(let [dirRow, dirCol] of [[-1, 0], [1, 0], [0, -1], [0, 1]]){
 			const nextRow = dirRow + curRow
 			const nextCol = dirCol + curCol
-			if(possible(nextRow, nextCol) && board[nextRow][nextCol] === 1 && !visited[nextRow][nextCol]){
-				visited[nextRow][nextCol] = true
+			if(possible(nextRow, nextCol) && board[nextRow][nextCol] === 1){
+				board[nextRow][nextCol] = 0
 				q.push([nextRow, nextCol])
 				size++
 			}
@@ -33,15 +30,11 @@ let count = 0
 let max = 0
 for(let i = 0 ; i < n; i++){
 	for(let j = 0 ; j < m; j++){
-		if(board[i][j] === 1 && !visited[i][j]){
-			visited[i][j] = true
-			let area = bfs(i, j)
-			max = Math.max(max, area)
+		if(board[i][j] === 1){
+			max = Math.max(max, bfs(i, j))
 			count++
 		}
-		
 	}
 }
-
 console.log(count)
 console.log(max)
