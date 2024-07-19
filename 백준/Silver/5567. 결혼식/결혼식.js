94c57d1dff3441a9b36735044307ab86f80e4203
jisunchung <65282581+jisunchung@users.noreply.github.com>
2024-07-19 18:24:20 +0900

    [Silver II] Title: 결혼식, Time: 144 ms, Memory: 12920 KB -BaekjoonHub

---
+++ 백준/Silver/5567. 결혼식/결혼식.js
@@ -0,0 +1,31 @@
+const input = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n")
+
+const n = +input.shift()
+const m = +input.shift()
+const graph = Array.from(Array(n+1), () => [])
+const visited = new Array(n+1).fill(false)
+visited[0] = true
+visited[1] = true
+
+for(let i = 0 ; i < m ; i++){
+	let [a,b] = input[i].split(" ").map(Number)
+	graph[a].push(b)
+	graph[b].push(a)
+}
+
+let result = 0
+let q = []
+q.push([1,0]) //start, count
+while(q.length !== 0){
+	let [now, count] = q.shift()
+	if(count === 2) break
+	for(let next of graph[now]){
+		if(!visited[next]){ //친구의 친구인 경우까지만 
+			visited[next] = true
+			q.push([next, count+1])
+			result++
+		}
+	}
+}
+
+console.log(result)
