ba8befaa45a25f471d8b2f99409498cea042cc6e
jisunchung <65282581+jisunchung@users.noreply.github.com>
2024-07-25 15:39:56 +0900

    [Silver V] Title: 수들의 합, Time: 180 ms, Memory: 10172 KB -BaekjoonHub

--- 백준/Silver/1789. 수들의 합/수들의 합.js
+++ 백준/Silver/1789. 수들의 합/수들의 합.js
@@ -1,11 +1,9 @@
-let N = Number(require("fs").readFileSync("/dev/stdin").toString().trim())
+let S = Number(require("fs").readFileSync("/dev/stdin").toString().trim())
 
-let count = 0
-let j = 1
-while(N >= 0){
-	N -= j
-	j++
-	count++
+let sum = 0
+let current = 0
+while(sum <= S){
+	current++
+	sum += current
 }
-
-console.log(N === 0 ? count : count-1)
+console.log(current-1)
