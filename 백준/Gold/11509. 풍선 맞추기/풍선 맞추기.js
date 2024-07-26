a1619573c6f7cd38edd1d975e0465a7c26ff13e0
jisunchung <65282581+jisunchung@users.noreply.github.com>
2024-07-26 17:39:56 +0900

    [Gold V] Title: 풍선 맞추기, Time: 528 ms, Memory: 105564 KB -BaekjoonHub

---
+++ 백준/Gold/11509. 풍선 맞추기/풍선 맞추기.js
@@ -0,0 +1,22 @@
+const input = require("fs")
+	.readFileSync("/dev/stdin")
+	.toString()
+	.trim()
+	.split("\n");
+
+const N = Number(input[0]);
+const arr = input[1].split(" ").map(Number);
+
+const arrow = new Array(Math.max(...arr) + 1).fill(0);
+let result = 0;
+
+for (let x of arr) {
+	if (arrow[x] > 0) {
+		arrow[x] -= 1;
+		arrow[x - 1] += 1;
+	} else {
+		arrow[x - 1] += 1;
+		result++;
+	}
+}
+console.log(result);
