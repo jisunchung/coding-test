d13af0870f2d18947d57e99d923a13319102048f
jisunchung <65282581+jisunchung@users.noreply.github.com>
2024-07-31 16:37:26 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 128 ms, Memory: 11112 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.js
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.js
@@ -2,12 +2,11 @@ const [N, K] = require("fs").readFileSync("/dev/stdin").toString().trim().split(
 
 const queue = Array.from({length: N}, (_, idx) => idx+1)
 const result = []
-let count = 1
+let start = 0
 
-while (queue.length > 0) {
-    let val = queue.shift()
-    if(count % K === 0) result.push(val)
-    else queue.push(val)
-    count++
+while (queue.length) {
+    start += K-1
+	if(start >= queue.length) start %= queue.length
+	result.push(queue.splice(start,1))
 }
 console.log(`<${result.join(", ")}>`)
