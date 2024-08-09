09663a1ed7692ac9d0444def74041512025f96ea
jisunchung <65282581+jisunchung@users.noreply.github.com>
2024-08-09 21:50:34 +0900

    [Gold V] Title: 빗물, Time: 92 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Gold/14719. 빗물/빗물.js
@@ -0,0 +1,29 @@
+const input = require("fs")
+	.readFileSync("/dev/stdin")
+	.toString()
+	.trim()
+	.split("\n");
+
+const [H, W] = input[0].split(" ").map(Number);
+const blocks = input[1].split(" ").map(Number);
+let left = 0;
+let right = W - 1;
+let left_max = blocks[left];
+let right_max = blocks[right];
+let total_water = 0;
+
+if (W === 0) return 0;
+
+while (left <= right) {
+	if (left_max <= right_max) {
+		total_water += Math.max(0, left_max - blocks[left]);
+		left++;
+		left_max = Math.max(left_max, blocks[left]);
+	} else {
+		total_water += Math.max(0, right_max - blocks[right]);
+		right--;
+		right_max = Math.max(right_max, blocks[right]);
+	}
+}
+
+console.log(total_water);
