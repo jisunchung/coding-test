9f974869ccff03bab8812fb36e8b7f7f5bd60329
jisunchung <65282581+jisunchung@users.noreply.github.com>
2025-01-06 23:10:23 -0800

    [level 1] Title: 가운데 글자 가져오기, Time: 0.08 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -0,0 +1,10 @@
+function solution(s) {
+    var answer = '';
+    var len = s.length
+    if(len % 2 === 0){
+        answer = s[len/2-1] + s[len/2]
+    }else{
+        answer = s[Math.floor(len/2)]
+    }
+    return answer;
+}
