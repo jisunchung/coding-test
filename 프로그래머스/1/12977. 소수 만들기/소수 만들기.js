84a4f8876d303fa1c2e257038401c467732c6cf1
jisunchung <65282581+jisunchung@users.noreply.github.com>
2025-01-20 20:15:42 -0800

    [level 1] Title: 소수 만들기, Time: 7.19 ms, Memory: 37.4 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -1,14 +1,15 @@
 function solution(nums) {
-    //3개를 골라서 더해서 
     const selected = []
     let answer = 0
     function dfs(depth, start){
         if(depth === 3){
-            // console.log(selected)
             const num = selected.reduce((a,b) => a+b,0)
             let check= true
             for(let i = 2; i <= Math.sqrt(num); i++){
-                if(num % i === 0) check = false
+                if(num % i === 0){
+                    check = false
+                    return
+                }
             }
             if(check) answer++
             return
