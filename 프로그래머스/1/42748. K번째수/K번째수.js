a60fdef2e73f57aae1836d27f8e6599fef852d53
jisunchung <65282581+jisunchung@users.noreply.github.com>
2025-03-19 06:20:43 -0700

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,6 +1,7 @@
 function solution(array, commands) {
-    return commands.map(command => {
-        const [i, j, k] = command
-        return array.slice(i-1, j).sort((a,b)=>a-b)[k-1]
-    })
+    return commands.map(([i, j ,k]) => 
+        array.slice(i-1,j).sort((a,b) => a-b)[k-1]
+    )
+
+
 }
