c0422674528cf3c820fcde97f692894e191331ad
jisunchung <65282581+jisunchung@users.noreply.github.com>
2025-03-19 23:58:59 -0700

    [level 2] Title: 다리를 지나는 트럭, Time: 0.54 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.js
@@ -1,21 +1,25 @@
 function solution(bridge_length, weight, truck_weights) {
-    let answer = 0
-    let q = Array(bridge_length).fill(0)
+    let time = 0
+    const bridge = []
     let sum = 0
-    while(truck_weights.length || sum > 0){
-        answer++
-        
-        sum -= q.shift()
-        
-        if(truck_weights.length > 0){
-            if(sum + truck_weights[0] <= weight){
-                let curr = truck_weights.shift()
-                q.push(curr)
-                sum += curr
-            }else{
-                q.push(0)
+    
+    while(truck_weights.length > 0 || bridge.length > 0){
+        if(bridge.length > 0 && bridge[0].time === time){
+            sum -= bridge.shift().weight
+        }
+        if(truck_weights.length > 0 && sum + truck_weights[0] <= weight){
+            const curr = truck_weights.shift()
+            sum += curr
+            bridge.push({
+                weight : curr, time : time + bridge_length
+            })
+        }else {
+            if(bridge.length > 0){
+                time = bridge[0].time-1
             }
         }
+        time++
     }
-    return answer
+    
+    return time
 }
