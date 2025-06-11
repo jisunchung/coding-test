bc88c08295ab050f2b1e15a36735f1b6ffaa525e
jisunchung <65282581+jisunchung@users.noreply.github.com>
2025-06-11 15:04:37 +0900

    [level 3] Title: 여행경로, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.js
+++ 프로그래머스/3/43164. 여행경로/여행경로.js
@@ -1,43 +1,28 @@
 function solution(tickets) {
 
-    //알파벳 순서가 앞서는 경로를 먼저 탐색하기 위해.. sort?
+    //알파벳 순서가 앞서는 경로를 먼저 탐색하기 위해
     tickets.sort()
     
     //map사용하기 departure : destinaion list[[des, index]...]
     const map = {}
-    const visited = new Array(tickets.length).fill(false)
     
     for(let i = 0; i < tickets.length; i++){ 
         const [dep,des] = tickets[i]
         if(!map[dep]) map[dep] = []
-        map[dep].push([des, i])
+        map[dep].push(des)
     }
     
     //dfs 실행, 방문 여부 확인해야됨
     //모두 사용했는지 어떻게...파악하지?
     let answer = []
-    const dfs = (dep, res) => {
-        //다 방문함
-        if(res.length === tickets.length + 1){
-            console.log(res)
-            answer = [...res]
-            return true
+    const dfs = (dep) => {
+        const des = map[dep]
+        while(des && des.length > 0){
+            dfs(des.shift())
         }
-
-        for(const [departure, index] of map[dep] || []){
-            console.log(departure, index)
-            
-            if(visited[index]) continue
-            res.push(departure)
-            visited[index] = true
-        
-            if(dfs(departure, res)) return true
-            res.pop()
-            visited[index] = false
-        }
-        return false
+        answer.push(dep)
     }
-    dfs("ICN", ["ICN"])
+    dfs("ICN")
     
-    return answer;
+    return answer.reverse();
 }
