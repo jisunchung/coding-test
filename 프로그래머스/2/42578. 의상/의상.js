a2cecbad490fd8d16c390c8f6a3b2a00e39bda6c
jisunchung <65282581+jisunchung@users.noreply.github.com>
2025-06-12 14:17:21 +0900

    [level 2] Title: 의상, Time: 0.28 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -1,7 +1,22 @@
 function solution(clothes) {
+    //1. 종류별로 hash map을 만든다 
+    //2. 종류별로 최대 1가지 의상만 착용 가능
+    //3. 일부가 겹쳐도 다른 의상이 다르면 됨
+    //4. 하나 이상은 입어야됨
+    
     const map = {}
-    for(const [clothe, option] of clothes){
-        map[option] = (map[option] || 0 ) +1
-    } 
-    return Object.values(map).reduce((acc, curr) => acc *(curr+1),1) -1
+    for(const [_, type] of clothes){
+        map[type] = (map[type] || 0) + 1
+    }
+    
+    let res = 1
+    for(const val in map){
+        res *= (map[val] + 1)
+    }
+    
+    return res - 1
+    
+    
+    
+    
 }
