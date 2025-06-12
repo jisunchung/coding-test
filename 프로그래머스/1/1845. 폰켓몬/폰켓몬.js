97f8564ee00a91aede430a21d9631fa1b32cb423
jisunchung <65282581+jisunchung@users.noreply.github.com>
2025-06-12 15:14:50 +0900

    [level 1] Title: 폰켓몬, Time: 0.75 ms, Memory: 34.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,5 +1,5 @@
 function solution(nums) {
-    const choose = Math.floor(nums.length / 2)
-    let s = new Set(nums)
-    return s.size > choose ? choose : s.size
+    //반을 가질 수 있을 때 가장 많은 포켓몬의 종류번호의 개수
+    const set = new Set(nums)
+    return set.size > nums.length / 2  ? nums.length  / 2 : set.size
 }
