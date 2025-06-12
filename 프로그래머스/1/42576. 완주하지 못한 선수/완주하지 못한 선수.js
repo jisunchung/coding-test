cc5a8bf90e3ecd6cc0aaadbcf4f40eeec87ab96e
jisunchung <65282581+jisunchung@users.noreply.github.com>
2025-06-12 13:29:38 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 93.02 ms, Memory: 71.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,13 +1,15 @@
 function solution(participant, completion) {
-    const map = new Map()
-    participant.forEach(parti => {
-        if(map.has(parti)) map.set(parti, map.get(parti)+1)
-        else map.set(parti, 1)
-    })
-    completion.forEach(comple => {
-        map.set(comple, map.get(comple)-1)
-        if(map.get(comple) === 0) map.delete(comple)
-    })
-    //return [...map][0][0] # [[key, value], ...]
-    return [...map.keys()][0]
+    const map = {}
+    for(const person of participant){
+        map[person] = (map[person] || 0) + 1
+    }
+    
+    for(const person of completion){
+        map[person] = map[person] - 1
+    }
+    
+    for(const val in map){
+        if(map[val] === 1) return val
+    }
+
 }
