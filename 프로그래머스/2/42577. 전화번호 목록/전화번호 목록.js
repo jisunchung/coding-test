7a873c579b19e0f692e569af704b805bbfba96dd
jisunchung <65282581+jisunchung@users.noreply.github.com>
2025-06-15 13:20:14 +0900

    [level 2] Title: 전화번호 목록, Time: 1667.63 ms, Memory: 449 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,9 +1,46 @@
+class TrieNode {
+    constructor() {
+        this.children = new Map()
+        this.endOfWord = false
+    }
+}
+
+class PrefixTree {
+    constructor() {
+        this.root = new TrieNode()
+    }
+
+    insertAndCheck(word) {
+        let cur = this.root
+        for (let c of word) {
+            // 이전 번호가 현재 번호의 접두어인 경우
+            if (cur.endOfWord) return false
+
+            if (!cur.children.has(c)) {
+                cur.children.set(c, new TrieNode())
+            }
+            cur = cur.children.get(c)
+        }
+
+        // 현재 번호가 이전 번호의 접두어인 경우
+        if (cur.children.size > 0) return false
+
+        cur.endOfWord = true
+        return true
+    }
+}
+
 function solution(phone_book) {
+    const prefixTree = new PrefixTree()
+
     phone_book.sort()
-    for(let i = 0; i < phone_book.length -1; i++){
-        if(phone_book[i+1].startsWith(phone_book[i])){
+    console.log(phone_book)
+
+    for (const number of phone_book) {
+        if (!prefixTree.insertAndCheck(number)) {
             return false
         }
     }
+
     return true
 }
