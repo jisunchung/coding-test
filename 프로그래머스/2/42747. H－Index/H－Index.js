80158f9fde0bb6bd2b8df1a0538414156e9ab75d
jisunchung <65282581+jisunchung@users.noreply.github.com>
2025-06-16 13:46:12 +0900

    [level 2] Title: H-Index, Time: 0.63 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,9 +1,6 @@
 function solution(citations) {
     citations.sort((a,b) => b-a)
-    for(let i = 0; i < citations.length; i++){
-        if(i+1 > citations[i]){
-            return i
-        }
-    }
-    return citations.length
+    let i = 0
+    while(i+1 <= citations[i]) i++
+    return i
 }
