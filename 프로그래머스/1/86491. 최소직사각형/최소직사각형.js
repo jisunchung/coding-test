65a4d237c449d46defc6a5584e6049ce81a2d80f
jisunchung <65282581+jisunchung@users.noreply.github.com>
2025-06-25 13:48:36 +0900

    [level 1] Title: 최소직사각형, Time: 14.15 ms, Memory: 42 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,9 +1,12 @@
 function solution(sizes) {
-    let max_w = 0
-    let max_h = 0
-    for([w,h] of sizes){
-        max_w = Math.max(max_w, Math.max(w,h))
-        max_h = Math.max(max_h, Math.min(w,h))
+    //가장 작은 지갑의 크기를 return
+    //[w,h]중에서 큰 값이 앞에오게 정렬
+    const tmp = sizes.map( i => i.sort((a,b) => a-b))
+    console.log(tmp)
+    let maxWith = 0, maxHeight = 0
+    for(const [w,h] of tmp){
+        maxWith = Math.max(maxWith, w)
+        maxHeight = Math.max(maxHeight, h)
     }
-    return max_w * max_h
+    return maxWith * maxHeight
 }
