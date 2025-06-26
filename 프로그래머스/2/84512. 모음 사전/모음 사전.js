49b50a922b6949243321f5c5757a2c42d540d340
jisunchung <65282581+jisunchung@users.noreply.github.com>
2025-06-26 16:25:26 +0900

    [level 2] Title: 모음 사전, Time: 0.88 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.js
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.js
@@ -1,26 +1,29 @@
 function solution(word) {
     let answer = 0;
     const vowels = [ 'A', 'E', 'I', 'O', 'U']
-    const res =[]
-    const dfs = (selected, depth, ans) => {
-        if(selected.length <= 5) {
-            ans[0]++
-            // console.log(selected.join(""))
-           if(selected.join("") === word){
-               answer = ans[0]
-            console.log("찾음",word, answer)
-               return 
-           }
-        }else return 
+    let found = false
 
-        for(let i = 0 ; i < vowels.length; i++){
-            selected.push(vowels[i])
-            dfs(selected, depth+1, ans) 
-            selected.pop()
+    const dfs = (current) => {
+        if (found) return;
+
+        if (current.length > 0) {
+          answer++;
+          if (current === word) {
+            found = true;
+            return;
+          }
         }
+
+        if (current.length === 5) return;
+
+        for (let i = 0; i < vowels.length; i++) {
+          dfs(current + vowels[i]);
+        }
+
     }
-    dfs([],0, [0])
-    return answer -1
+    
+    dfs("")
+    return answer
     
 
 }
